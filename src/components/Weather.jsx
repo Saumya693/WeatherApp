84365caf32d8956d9e9e5948db5a0a6c3f88fe31
Saumya Shukla <saumyashukla56013@gmail.com>
2025-07-22 18:26:24 +0530

    Initial commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./Weather.css";
+import search_icon from "../assets/search.png";
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzle_icon from "../assets/drizzle.png";
+import humidity_icon from "../assets/humidity.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
+
+const apiId = import.meta.env.VITE_APP_ID ?? "0546e22e96a637199bf89a14bb8513c8";
+
+const Weather = () => {
+  const [weatherData, setWeatherData] = useState(false);
+  const inputRef = useRef();
+
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02d": cloud_icon,
+    "03d": cloud_icon,
+    "03d": cloud_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
+  };
+  const search = async (city) => {
+    if (city === "") {
+      toast("Enter City Name");
+      return;
+    }
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=matric&appid=${apiId}`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      const icon = allIcons[data.weather[0].icon] || clear_icon;
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: icon,
+      });
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    search("Lucknow");
+  }, []);
+
+  return (
+    <div className="weather">
+      <div className="search-bar">
+        <input ref={inputRef} type="text" placeholder="Search" />
+        <img
+          src={search_icon}
+          alt=""
+          onClick={() => search(inputRef.current.value)}
+        />
+      </div>
+      <img src={weatherData.icon} alt="" className="weather_icon" />
+      <p className="temprature">{weatherData.temperature}°c</p>
+      <p className="location">{weatherData.location}</p>
+      <div className="weather_data">
+        <div className="col">
+          <img src={humidity_icon} alt="" />
+          <div>
+            <p>{weatherData.humidity} %</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src={wind_icon} alt="" />
+          <div>
+            <p>{weatherData.windSpeed}km</p>
+            <span>Wind Speed</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
---
+++ src/services/WeatherAPI.jsx
@@ -0,0 +1,11 @@
+// import axios from "axios";
+
+// const weatherApi = axios.create({
+//   baseURL: "https://api.openweathermap.org/data/2.5",
+// });
+
+// const API_KEY = import.meta.env.VITE_API_KEY; // from .env
+
+// export const getWeatherByCity = (cityName) => {
+//   return weatherApi.get(`/weather?q=${cityName}&appid=${API_KEY}&units=metric`);
+// };
